12a39c5f700d1ed6b99cef54348a05a1d559b7f9
Dylan <BooshBear03@gmail.com>
2023-05-18 01:42:55 -0700

    New Style

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function FrontPage() {
           alt='My Image'
           />
         </div>
-        <div className={styles.colL}>
+        <div className={styles.general}>
           <h1>General Info</h1>
           <ul className="dataList">
             <li><span className={styles.dataArrows}>&gt;</span>Individual signups</li>
@@ -20,7 +20,7 @@ export default function FrontPage() {
             <li><span className={styles.dataArrows}>&gt;</span>Top teams make playoffs</li>
           </ul>
         </div>
-        <div className={styles.colM}>
+        <div className={styles.culture}>
           <h1>Player Culture</h1>
           <ul className="dataList">
             <li><span className={styles.dataArrows}>&gt;</span>Make new friends</li>
@@ -29,7 +29,7 @@ export default function FrontPage() {
             <li><span className={styles.dataArrows}>&gt;</span>Play with all skill levels</li>
           </ul>
         </div>
-        <div className={styles.colR}>
+        <div className={styles.features}>
           <h1>RD2L Features</h1>
           <ul className="dataList">
             <li><span className={styles.dataArrows}>&gt;</span>Participate in inhouses</li>
@@ -37,6 +37,18 @@ export default function FrontPage() {
             <li><span className={styles.dataArrows}>&gt;</span>Mini leagues between seasons</li>
             <li><span className={styles.dataArrows}>&gt;</span>Find Battlecup stacks</li>
           </ul>
+        </div>
+        <div className={styles.teams}>
+          <h1>RD2L Teams</h1>
+          <ul className="dataList">
+            <li><span className={styles.dataArrows}>&gt;</span>Participate in inhouses</li>
+            <li><span className={styles.dataArrows}>&gt;</span>Play in weekend tourneys</li>
+            <li><span className={styles.dataArrows}>&gt;</span>Mini leagues between seasons</li>
+            <li><span className={styles.dataArrows}>&gt;</span>Find Battlecup stacks</li>
+          </ul>
+        </div>
+        <div className={styles.join}>
+          <h1>Join Now</h1>
         </div>
       </div>
     </main>
