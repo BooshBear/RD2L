b9cded25e0a21cd0f5aa5ac8aaf2fa917c9b19da
Dylan.Mailloux <Dsisson0505@gmail.com>
2024-01-17 14:49:54 -0800

    changed the divisions to be at the top

--- rd2l-site/src/app/page.tsx
+++ rd2l-site/src/app/page.tsx
@@ -3,7 +3,28 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      <div className='grid grid-colg-[25% 50% 25%] grid-rows-2 justify-center items-center'>
+      <div className='grid grid-colg-[25% 50% 25%] justify-center items-center'>
+        <div className='mb-20 col-span-3 flex flex-col justify-center items-center'>
+          <h1 className='uppercase font-mono text-xl'>Divisions</h1>
+          <div className='flex justify-between w-full font-sans'>
+            <div className='flex flex-col justify-center items-center'>
+              <h1 className='underline'>Monday</h1>
+              <p>Season 33</p>
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+              <h1 className='underline'>Tuesday</h1>
+              <p>Season 33</p>
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+              <h1 className='underline'>Saturday</h1>
+              <p>Season 33</p>
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+              <h1 className='underline'>Sunday</h1>
+              <p>Season 33</p>
+            </div>
+          </div>
+        </div>
         <div className='col-start-2 row-span-2'>
           <Image
           className='relative drop-shadow-[0_0_6rem_#ffffff70] opacity-50'
@@ -14,7 +35,7 @@ export default function Home() {
           priority
           />
         </div>
-        <div className='row-start-1 flex flex-col justify-center items-center'>
+        <div className='row-start-2 flex flex-col justify-center items-center'>
           <h1 className='uppercase font-mono text-xl mb-2'>General Info</h1>
           <ul className='font-sans text-lg'>
             <li><span className='text-[#9b3839] font-bold text-xl mr-4'>&gt;</span>Individual signups</li>
@@ -50,27 +71,7 @@ export default function Home() {
             <li><span className='text-[#9b3839] font-bold text-xl mr-4'>&gt;</span>Find Battlecup stacks</li>
           </ul>
         </div>
-        <div className='col-span-3 flex flex-col justify-center items-center'>
-          <h1 className='mt-20 uppercase font-mono text-xl'>Divisions</h1>
-          <div className='flex justify-between w-full font-sans'>
-            <div className='flex flex-col justify-center items-center'>
-              <h1 className='underline'>Monday</h1>
-              <p>Season 33</p>
-            </div>
-            <div className='flex flex-col justify-center items-center'>
-              <h1 className='underline'>Tuesday</h1>
-              <p>Season 33</p>
-            </div>
-            <div className='flex flex-col justify-center items-center'>
-              <h1 className='underline'>Saturday</h1>
-              <p>Season 33</p>
-            </div>
-            <div className='flex flex-col justify-center items-center'>
-              <h1 className='underline'>Sunday</h1>
-              <p>Season 33</p>
-            </div>
-          </div>
-        </div>
+      
       </div>
     </main>
   )
