b7d9081cb2d7a45e84cff8726d5058500917fe25
Dylan.Mailloux <Dsisson0505@gmail.com>
2024-02-01 11:13:21 -0800

    Updated profile page with conditional

--- rd2l-site/src/app/profile/page.tsx
+++ rd2l-site/src/app/profile/page.tsx
@@ -1,8 +1,15 @@
+"use client"
 import { useSession } from "next-auth/react"
+import { Redirect } from "next"
 
 export default function Profile() {
     const {data: session} = useSession()
-    return (
-        session
+    return session? 
+    (
+        <></>
+    )
+    :
+    (
+        <></>
     )
 }
