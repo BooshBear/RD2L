a7f6e5d9bf13c13867eac9112dd4a9c63a42f1de
Dylan.Mailloux <Dsisson0505@gmail.com>
2024-02-15 13:03:32 -0800

    added in routes folder to set up logged in routing and stuff, Next plan is to add a better file structure so that things can be reused like links to sign in for google and reddit and other stuff

--- rd2l-site/src/app/api/auth/[...nextauth]/route.tsx
+++ rd2l-site/src/app/api/auth/[...nextauth]/route.tsx
@@ -62,6 +62,13 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
           })
     ],
+    pages: {
+      signIn: '/login',
+      signOut: '/auth/signout',
+      error: '/auth/error', // Error code passed in query string as ?error=
+      verifyRequest: '/auth/verify-request', // (used for check email message)
+      newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+    },
     secret: process.env.NEXTAUTH_SECRET as string,
 })
 
