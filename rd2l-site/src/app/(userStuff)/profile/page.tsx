bc223786419dd00c579d0d44508b08be09814c29
Dylan.Mailloux <Dsisson0505@gmail.com>
2024-02-28 13:07:07 -0800

    build testing 2

--- rd2l-site/src/app/(userStuff)/profile/page.tsx
+++ rd2l-site/src/app/(userStuff)/profile/page.tsx
@@ -2,7 +2,7 @@ export default function Profile() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-10">
             <div> 
-                <h1>testing page</h1>
+                <h1>testing pae</h1>
             </div>
         </main>
     )
