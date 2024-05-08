61a08b3df91ee271ff401760b18d34c3ac453841
Dylan.Mailloux <Dsisson0505@gmail.com>
2024-05-08 12:08:00 -0700

    added interface for player

--- rd2l-site/src/app/(mainPages)/players/page.tsx
+++ rd2l-site/src/app/(mainPages)/players/page.tsx
@@ -1,10 +1,16 @@
 'use client'
 import React, { useState } from 'react';
 
+interface Player {
+    id: number;
+    name: string;
+    // Add other properties as needed
+}
+
 const Players = () => {
     const [searchQuery, setSearchQuery] = useState('');
-    const [playersData, setPlayersData] = useState([]);  // You would need to fetch and set your player data here
-    const [filteredPlayers, setFilteredPlayers] = useState([]);  // State for filtered player data
+    const [playersData, setPlayersData] = useState<Player[]>([]);  // You would need to fetch and set your player data here
+    const [filteredPlayers, setFilteredPlayers] = useState<Player[]>([]);  // State for filtered player data
 
     const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         const query = event.target.value;
@@ -16,7 +22,7 @@ const Players = () => {
         );
         setFilteredPlayers(filteredPlayers);
     };
-
+    
     return (
         <main className="flex min-h-screen flex-col items-center p-10 gap-2">
             <div>
