2d17ea8bc92adc5d972eb061776cab4d4bb4e65b
Dylan.Mailloux <Dsisson0505@gmail.com>
2024-05-08 12:56:38 -0700

    Adding in new login for just steam openid only

--- rd2l-site/src/app/api/auth/[...nextauth]/route.tsx
+++ rd2l-site/src/app/api/auth/[...nextauth]/route.tsx
@@ -1,98 +1,33 @@
-import NextAuth from "next-auth/next";
-import CredentialsProvider from "next-auth/providers/credentials"
-import RedditProvider from "next-auth/providers/reddit";
-import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "../../../../lib/mongoDBConnect";
+import NextAuth from 'next-auth';
+import SteamProvider, {PROVIDER_ID} from 'next-auth-steam';
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-
-const handler = NextAuth({
-    // @ts-ignore
-    adapter: MongoDBAdapter(clientPromise),
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  return NextAuth(req, res, {
     providers: [
-      {
-        id: 'steam',
-        name: 'Steam',
-        type: 'oauth',
-        accessTokenUrl: 'https://steamcommunity.com/openid/login',
-        requestTokenUrl: 'https://steamcommunity.com/openid/login',
-        authorization: 'https://steamcommunity.com/openid/login',
-        profileUrl: 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/',
-        profile: (profile:any) => {
-          return {
-            id: profile.steamid,
-            name: profile.personaname,
-            email: profile.email,
-            image: profile.avatar
-          };
-        },
-        clientId: process.env.STEAM_CLIENT_ID,
-        clientSecret: process.env.STEAM_CLIENT_SECRET,
-      },
-      CredentialsProvider({
-        // The name to display on the sign in form (e.g. 'Sign in with...')
-        name: 'Sign in with Credentials',
-        // The credentials is used to generate a suitable form on the sign in page.
-        // You can specify whatever fields you are expecting to be submitted.
-        // e.g. domain, username, password, 2FA token, etc.
-        // You can pass any HTML attribute to the <input> tag through the object.
-        credentials: {
-          username: { label: "Username", type: "text", placeholder: "username" },
-          password: { label: "Password", type: "password", placeholder: "password" },
-          steamID: { label: "SteamID", type: "steamID", placeholder: "steamID" }
-        },
-        async authorize(credentials, req) {
-          // You need to provide your own logic here that takes the credentials
-          // submitted and returns either a object representing a user or value
-          // that is false/null if the credentials are invalid.
-          // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-          // You can also use the `req` object to obtain additional parameters
-          // (i.e., the request IP address)
-          if (!credentials?.username || !credentials?.password) return null;
-          try {
-            // @ts-ignore
-            const user = await login(credentials.username, credentials.password);
-            return user;
-          } catch (e) {
-            console.error(e);
-          }
-          const res = await fetch("/your/endpoint", {
-            method: 'POST',
-            body: JSON.stringify(credentials),
-            headers: { "Content-Type": "application/json" }
-          })
-          const user = await res.json()
-    
-          // If no error and we have user data, return it
-          if (res.ok && user) {
-            return user
-          }
-          // Return null if user data could not be retrieved
-          return null
-        }
-      }),
-        RedditProvider({
-            clientId: process.env.REDDIT_CLIENT_ID as string,
-            clientSecret: process.env.REDDIT_CLIENT_SECRET as string,
-            authorization: {
-              params: {
-                duration: "permanent",
-              },
-            },
-          }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-          })
+      SteamProvider(req, {
+        clientSecret: process.env.STEAM_SECRET!,
+        callbackUrl: 'http://localhost:3000/api/auth/callback'
+      })
     ],
-    pages: {
-      signIn: '/login',
-      signOut: '/auth/signout',
-      error: '/auth/error', // Error code passed in query string as ?error=
-      verifyRequest: '/auth/verify-request', // (used for check email message)
-      newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-    },
-    secret: process.env.NEXTAUTH_SECRET as string,
-})
+    callbacks: {
+      jwt({ token, account, profile }) {
+        if (account?.provider === PROVIDER_ID) {
+          token.steam = profile
+        }
+
+        return token
+      },
+      session({ session, token }) {
+        if ('steam' in token) {
+          // @ts-expect-error
+          session.user.steam = token.steam
+        }
+
+        return session
+      }
+    }
+  })
+}
 
 export {handler as GET, handler as POST}
