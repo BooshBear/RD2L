21b2c9e766150446a5a3468aa98e50ab1f923250
Dylan.Mailloux <Dsisson0505@gmail.com>
2024-05-14 20:13:03 -0700

    try url fix2

--- rd2l-site/src/app/api/auth/[...nextauth]/route.ts
+++ rd2l-site/src/app/api/auth/[...nextauth]/route.ts
@@ -12,7 +12,7 @@ interface RouteHandlerContext {
 async function handler(req: NextRequest, context: RouteHandlerContext) {
   const callbackUrl =
       process.env.NODE_ENV === 'production'
-        ? 'https://rd2l.vercel.app/api/auth/callback'
+        ? 'https://rd2l.vercel.app/'
         : 'http://localhost:3000/api/auth/callback';
   try {
     
