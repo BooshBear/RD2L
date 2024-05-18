ade10a13c2288d615cab02eebb90caa410ccb37e
Dylan.Mailloux <Dsisson0505@gmail.com>
2024-05-18 12:04:58 -0700

    try fix 11

--- rd2l-site/src/app/api/players/route.ts
+++ rd2l-site/src/app/api/players/route.ts
@@ -1,7 +1,6 @@
 'use server'
 import { NextRequest, NextResponse } from 'next/server'
 import clientPromise from '@/lib/mongoDBConnect';
-export const dynamic = 'force-dynamic'
 
 export async function GET(req: NextRequest, res: NextResponse) {
     console.log("Received GET request to /api/players");
