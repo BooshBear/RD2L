52f72555c5adfae3a0a861aab5b7c1cd826b3515
Dylan.Mailloux <Dsisson0505@gmail.com>
2024-05-18 11:57:32 -0700

    force making players dynamic

--- rd2l-site/src/app/(mainPages)/players/page.tsx
+++ rd2l-site/src/app/(mainPages)/players/page.tsx
@@ -1,6 +1,9 @@
 import FetchPlayers from "@/components/fetchplayers";
 
+export const dynamic = 'force-dynamic'
+
 export default function Players() {
+
     return (
         <main className="flex min-h-screen flex-col items-center p-10 gap-2">
             <FetchPlayers></FetchPlayers>
